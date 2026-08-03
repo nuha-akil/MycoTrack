b3a76145eea00cfa1b26c8fb1a2b8dd3c4b1f9b9
Shimlashafeek43 <shafeek.fathimashimla@gmail.com>
2026-08-03 22:54:49 +0530

    created successful message screens new password screen password change screen deleted screen logout screen

---
+++ lib/screens/Successful messages/successfully_deleted_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class LogoutScreen extends StatelessWidget {
+  const LogoutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF7ECD6), // Cream background
+      body: Center(
+        child: Container(
+          width: 300,
+          padding: const EdgeInsets.symmetric(
+            vertical: 30,
+            horizontal: 20,
+          ),
+          decoration: BoxDecoration(
+            color: const Color(0xFF7B5244), // Brown
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: const Text(
+            "Successfully Deleted!!!",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
