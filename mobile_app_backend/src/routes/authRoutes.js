f5c461ebf5570e43baccbd8368287ee0f4f4b668
nuha-akil <nuhaakil.2002@gmail.com>
2026-08-06 03:10:10 +0530

    Add password recovery API routes

--- mobile_app_backend/src/routes/authRoutes.js
+++ mobile_app_backend/src/routes/authRoutes.js
@@ -3,11 +3,18 @@ const express = require("express");
 const {
   registerUser,
   loginUser,
+  checkResetEmail,
+  resetPasswordDirectly,
 } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/check-reset-email", checkResetEmail);
+router.post(
+  "/reset-password-direct",
+  resetPasswordDirectly
+);
 
 module.exports = router;
